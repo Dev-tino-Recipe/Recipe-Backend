b8538cb04f4eb221a0f5ff414ad6df3c2c216cd7
JeongYoonPark <50254300+JeongYoonPark@users.noreply.github.com>
2024-09-29 14:39:21 +0900

    temp commit

--- src/controller/bookmarkController.js
+++ src/controller/bookmarkController.js
@@ -1,22 +1,42 @@
 import express from "express";
 import bookmarkRepository from "../repository/bookmarkRepository.js";
-import {returnResponse} from "../utils/response.js";
+import { returnResponse } from "../utils/response.js";
+import userRepository from "../repository/userRepository.js";
+import recipeRepository from "../repository/recipeRepository.js";
+import apiResponse from "../dto/apiResponse.js";
 
 const bookmarkController = express.Router();
 
-// bookmarkController.post("/regist", async (req, res, next) => {
-//   const { user_id, recipe_id } = req.body;
-//   try {
-//     const insert = await bookmarkRepository.createBookmark(user_id, recipe_id);
-//     if (insert.affectedRows) {
-//       res
-//         .status(200)
-//         .json({ is_Success: true, message: "북마크 등록에 성공했습니다." });
-//     }
-//   } catch (e) {
-//     next(e);
-//   }
-// });
+bookmarkController.post("/regist", async (req, res, next) => {
+  const { userId, recipeId } = req.body;
+  try {
+    const findUser = await userRepository.findById(userId);
+    console.log(findUser);
+    if (!findUser) {
+      res.status(404).json(
+        apiResponse.failure({
+          message: "존재하지 않는 유저입니다.",
+        }),
+      );
+    }
+    const findRecipe = await recipeRepository.findById(recipeId);
+    if (!findRecipe) {
+      res.status(404).send(
+        apiResponse.failure({
+          message: "존재하지 않는 레시피입니다.",
+        }),
+      );
+    }
+    const insert = await bookmarkRepository.createBookmark(userId, recipeId);
+    if (insert.affectedRows) {
+      res
+        .status(200)
+        .json({ is_Success: true, message: "북마크 등록에 성공했습니다." });
+    }
+  } catch (e) {
+    next(e);
+  }
+});
 //
 // bookmarkController.delete("/delete", async (req, res, next) => {
 //   const { user_id, recipe_id } = req.body;
--- src/repository/bookmarkRepository.js
+++ src/repository/bookmarkRepository.js
@@ -2,35 +2,37 @@ import conn from "../db/connection.js";
 
 export default {
   async findByRecipeIdAndUserId(recipeId, userId, connection) {
-    const findBookMarks = await conn.query(`
+    const findBookMarks = await conn.query(
+      `
         SELECT *
         from bookmark
         where recipeId = ?
           and userId = ?
-    `, [recipeId, userId], connection)
+    `,
+      [recipeId, userId],
+      connection,
+    );
 
     return findBookMarks[0];
-  }
+  },
 
-  // createBookmark: async (user_id, recipe_id) => {
-  //   const bookmark_id = generate_uuid();
-  //   const insertBookmark = await conn.query(
-  //     `INSERT INTO Bookmark
-  //       (
-  //           bookmark_id,
-  //           user_id,
-  //           recipe_id
-  //       )
-  //       VALUES (?, ?, ?)`,
-  //     [bookmark_id, user_id, recipe_id],
-  //   );
-  //   return insertBookmark;
-  // },
-  // deleteBookmark: async (user_id, recipe_id) => {
-  //   const delBookmark = await conn.query(
-  //     `DELETE FROM Bookmark WHERE user_id = ? AND recipe_id = ?`,
-  //     [user_id, recipe_id],
-  //   );
-  //   return delBookmark;
-  // },
+  createBookmark: async (user_id, recipe_id) => {
+    const insertBookmark = await conn.query(
+      `INSERT INTO Bookmark
+        (
+            userId,
+            recipeId
+        )
+        VALUES (?, ?)`,
+      [user_id, recipe_id],
+    );
+    return insertBookmark;
+  },
+  deleteBookmark: async (user_id, recipe_id) => {
+    const delBookmark = await conn.query(
+      `DELETE FROM Bookmark WHERE user_id = ? AND recipe_id = ?`,
+      [user_id, recipe_id],
+    );
+    return delBookmark;
+  },
 };
