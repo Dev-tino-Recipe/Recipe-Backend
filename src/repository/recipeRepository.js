e195724f7a4c28a82608ac6ccf156c1ab1a57928
JeongYoonPark <50254300+JeongYoonPark@users.noreply.github.com>
2024-10-02 12:45:07 +0900

    merge from develop

--- src/repository/recipeRepository.js
+++ src/repository/recipeRepository.js
@@ -42,40 +42,6 @@ export default {
         where recipeId = ?
     `;
     return (await conn.query(recipeSql, [recipeId], connection))[0];
-
-    // const findUser = await conn.query(`
-    //
-    // `);
-    //
-    //
-    // const reu = await conn.query(
-    //     `SELECT r.title as title, r.description as description, r.created_at as created_at, u.user_name as user_name
-    //      FROM recipes as r
-    //               LEFT JOIN users as u on u.user_id = r.user_id
-    //      WHERE r.recipe_id = ?`,
-    //     [recipeId],
-    // );
-
-    // return {
-    //   recipeDetails: {
-    //     title: reu[0].title,
-    //     description: reu[0].description,
-    //     created_at: reu[0].created_at,
-    //     user: {
-    //       user_name: reu[0].user_name,
-    //     },
-    //   },
-    //   instructions: res.map((row) => ({
-    //     title: row.title,
-    //     imgUrl: row.img_url,
-    //     description: row.description,
-    //     step_order: row.step_order,
-    //   })),
-    //   ingredients: ret.map((row) => ({
-    //     name: row.name,
-    //     quantity: row.quantity,
-    //   })),
-    // };
   },
   update: async (recipeId, title, thumbnail, description, connection) => {
     const updateRecipeSql = `
@@ -96,54 +62,21 @@ export default {
     `;
   },
 
-  // getRecipe: async (recipeId) => {
-  //   const res = await conn.query(
-  //       `SELECT title, img_url, description, step_order
-  //        FROM Instructions
-  //        WHERE recipe_id = ?`,
-  //       [recipeId],
-  //   );
-  //   const ret = await conn.query(
-  //       `SELECT name, quantity
-  //        FROM Ingredient
-  //        WHERE recipe_id = ?`,
-  //       [recipeId],
-  //   );
-  //   const reu = await conn.query(
-  //       `SELECT r.title as title, r.description as description, r.created_at as created_at, u.user_name as user_name
-  //        FROM recipes as r
-  //                 LEFT JOIN users as u on u.user_id = r.user_id
-  //        WHERE r.recipe_id = ?`,
-  //       [recipeId],
-  //   );
-  //
-  //   return {
-  //     recipeDetails: {
-  //       title: reu[0].title,
-  //       description: reu[0].description,
-  //       created_at: reu[0].created_at,
-  //       user: {
-  //         user_name: reu[0].user_name,
-  //       },
-  //     },
-  //     instructions: res.map((row) => ({
-  //       title: row.title,
-  //       imgUrl: row.img_url,
-  //       description: row.description,
-  //       step_order: row.step_order,
-  //     })),
-  //     ingredients: ret.map((row) => ({
-  //       name: row.name,
-  //       quantity: row.quantity,
-  //     })),
-  //   };
-  // },
-  // getRecentRecipe: async (recipeId, placeholder) => {
-  //   const sql = `SELECT thumbnail, title, description
-  //                FROM Recipes
-  //                WHERE recipe_id IN (${placeholder})`;
-  //
-  //   const res = await conn.query(sql, recipeId);
-  //   return res;
-  // },
+  findBookmarkRecipes: async (userId, page, pageSize, connection) => {
+    const skipStr = ((page - 1) * pageSize).toString();
+    const pageSizeStr = pageSize.toString();
+
+    const recipeSql = `
+        SELECT r.recipeId , r.title, r.thumbnail, r.description, r.updatedAt 
+        FROM recipes as r 
+        LEFT JOIN bookmark as b on b.recipeId = r.recipeId 
+        WHERE b.userId = ?
+        LIMIT ?, ?;
+    `;
+    return await conn.query(
+      recipeSql,
+      [userId, skipStr, pageSizeStr],
+      connection,
+    );
+  },
 };
